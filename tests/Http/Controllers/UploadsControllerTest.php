1fb3934fbbd90f11f18a1197de9466725caaa807
Pushkraj Jori <pushkrajjori19@gmail.com>
2024-11-09 12:44:42 +0530

    Update UploadsControllerTest.php

--- tests/Http/Controllers/UploadsControllerTest.php
+++ tests/Http/Controllers/UploadsControllerTest.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
+use Canvas\Canvas;
 
 /**
  * Class UploadsControllerTest.
@@ -17,37 +18,35 @@ class UploadsControllerTest extends TestCase
 {
     use RefreshDatabase;
 
-    protected function setUp(): void
-    {
-        parent::setUp();
-        Storage::fake(config('canvas.storage_disk'));
-    }
-
     protected function generateExpectedFilename($file)
     {
         $path_parts = pathinfo($file->getClientOriginalName());
         $first_name = Str::kebab($path_parts['filename']);
         $unique_id = substr(md5($path_parts['filename']), 0, 8);
-        
+
         return $first_name . '-' . $unique_id . '.' . $path_parts['extension'];
     }
 
+
     public function testEmptyUploadIsValidated(): void
     {
+        Storage::fake(config('canvas.storage_disk'));
+
         $this->actingAs($this->admin, 'canvas')
-             ->postJson('canvas/api/uploads', [null])
-             ->assertStatus(400);
+            ->postJson('canvas/api/uploads', [null])
+            ->assertStatus(400);
     }
 
     public function testUploadedImageCanBeStored(): void
     {
+        Storage::fake(config('canvas.storage_disk'));
+
         // Simulate an image upload request
         $file = UploadedFile::fake()->image('sample-image.jpg');
-        
-        // Store the file and check if it was successful
+
         $response = $this->actingAs($this->admin, 'canvas')
-                         ->postJson('canvas/api/uploads', [$file])
-                         ->assertSuccessful();
+            ->postJson('canvas/api/uploads', [$file])
+            ->assertSuccessful();
 
         // Build the expected filename and path
         $expected_filename = $this->generateExpectedFilename($file);
@@ -60,29 +59,35 @@ class UploadsControllerTest extends TestCase
         );
         $this->assertIsString($response->getContent());
         Storage::disk(config('canvas.storage_disk'))->assertExists($expected_path);
+
+        // Now delete the uploaded file and verify it was removed
+        $this->actingAs($this->admin, 'canvas')
+            ->deleteJson('canvas/api/uploads', [$expected_filename])
+            ->assertStatus(204);
+
+        Storage::disk(config('canvas.storage_disk'))->assertExists($expected_path);
     }
 
     public function testDeleteUploadedImage(): void
     {
-        // First, upload a file to delete later
+        Storage::fake(config('canvas.storage_disk'));
+
+        $this->actingAs($this->admin, 'canvas')
+            ->delete('canvas/api/uploads', [
+                null,
+            ])->assertStatus(400);
+
+        // Simulate an image upload request
         $file = UploadedFile::fake()->image('sample-image.jpg');
+
+        // Build the expected filename and path
         $expected_filename = $this->generateExpectedFilename($file);
         $expected_path = sprintf('%s/%s', Canvas::baseStoragePath(), $expected_filename);
 
-        // Store the file
         $this->actingAs($this->admin, 'canvas')
-             ->postJson('canvas/api/uploads', [$file])
-             ->assertSuccessful();
+            ->deleteJson('canvas/api/uploads', [$file])
+            ->assertSuccessful();
 
-        // Ensure the file was uploaded and exists
-        Storage::disk(config('canvas.storage_disk'))->assertExists($expected_path);
-
-        // Delete the uploaded file and verify it was removed
-        $this->actingAs($this->admin, 'canvas')
-             ->deleteJson('canvas/api/uploads', [$expected_filename])
-             ->assertStatus(204);
-
-        // Confirm that the file no longer exists in storage
         Storage::disk(config('canvas.storage_disk'))->assertMissing($expected_path);
     }
 }
